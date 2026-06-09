72cb71d640fac811b6861c46d5e52c46af9a2dfe
Rui Magato <quhuliu4@gmail.com>
2026-06-09 15:42:47 +0800

    added window and frame

--- src/main/java/com/ets/gui/AdminFrame.java
+++ src/main/java/com/ets/gui/AdminFrame.java
@@ -1,4 +1,25 @@
 package com.ets.gui;
 
-public class AdminFrame {
+import javax.swing.*;
+
+public class AdminFrame extends JFrame {
+    public AdminFrame() {
+        setTitle("Admin Frame");
+        setSize(400, 300);
+        setLocationRelativeTo(null);
+        // finish application with exit button
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        add(panel);
+    }
+
+    public static void main(String[] args){
+
+//        Create GUI on the Event Dispatch Thread
+        SwingUtilities.invokeLater(() -> {
+            AdminFrame adminFrame = new AdminFrame();
+            adminFrame.setVisible(true);
+        });
+    }
 }
