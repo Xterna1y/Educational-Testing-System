553175b8dbdb921869aca27692add885f343b094
Rui Magato <quhuliu4@gmail.com>
2026-06-10 17:48:51 +0800

    The class extends User.java. User.java is an abstruct class. there are three attributes, name, password, and role

--- src/main/java/com/ets/model/Admin.java
+++ src/main/java/com/ets/model/Admin.java
@@ -1,4 +1,7 @@
 package com.ets.model;
 
-public class Admin {
+public class Admin extends User {
+    public Admin(String adminName, String password, String role) {
+        super(adminName, password, "ADMIN");
+    }
 }
