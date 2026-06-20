6cc1fcce0e5bfc16ffea35a51b51a295499156bb
Rui Magato <quhuliu4@gmail.com>
2026-06-20 13:02:07 +0800

    initial screen

--- src/main/java/com/ets/gui/ResultFrame.java
+++ src/main/java/com/ets/gui/ResultFrame.java
@@ -1,4 +1,24 @@
 package com.ets.gui;
 
-public class ResultFrame {
+import javax.swing.*;
+import java.awt.*;
+
+public class ResultFrame extends JFrame {
+    private static final Color BG_DARK      = Color.BLACK;
+    private static final Color CARD_BG      = Color.DARK_GRAY;
+    private static final Color ACCENT       = Color.BLUE;
+    private static final Color TEXT_PRIMARY = Color.WHITE;
+    private static final Color TEXT_MUTED   = Color.LIGHT_GRAY;
+    private final String username;
+
+
+    public ResultFrame(String username) {
+        this.username = username;
+        buildUI();
+    }
+
+    private void buildUI() {
+        setTitle("ResultFrame");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
